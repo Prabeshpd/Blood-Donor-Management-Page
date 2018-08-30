877633a8ef4a4a00768e50b4336aa9fe88a1a197
prabeshpd <prabesh1995nepalktm@gmail.com>
2018-08-30 12:46:16 +0545

    admin user was created to manage donors extending base class

---
+++ User_management_system/adminuser.php
@@ -0,0 +1,22 @@
+<?php
+    class AdminUser extends BaseClass
+    {
+        protected static $db_table = "admin";
+        protected static $db_fields = array('email','password');
+        public $id;
+        public $email;
+        public $password;
+
+        public static function verify_user($email,$password)
+        {
+            global $database;
+            $email = $database->escape_string($email);
+            $password = $database->escape_string($password);
+            $sql = "SELECT * FROM " . self::$db_table . " WHERE ";
+            $sql .= "email = '{$email}' ";
+            $sql .= "AND password = '{$password}'";
+            $the_result_array = self::find_by_query($sql);
+            return !empty($the_result_array) ? array_shift($the_result_array) : false;
+        }
+    }
+?>
