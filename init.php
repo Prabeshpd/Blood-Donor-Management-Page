f46a8b4ee4ab4d9fd348114a6199a707c4c70752
prabeshpd <prabesh1995nepalktm@gmail.com>
2018-08-30 16:54:09 +0545

    Included all the important pages that needs to be called in every page in one page

---
+++ init.php
@@ -0,0 +1,8 @@
+<?php
+require_once("./config/config.php");
+require_once ("./config/database.php");
+require_once("./User_management_system/donor.php");
+require_once ("./User_management_system/adminuser.php");
+require_once ("./User_management_system/baseclass.php");
+require_once ("./session_manage/session.php");
+?>
