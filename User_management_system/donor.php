08451e495e480dfdc6d42002b3eae57d010e8542
prabeshpd <prabesh1995nepalktm@gmail.com>
2018-08-31 20:22:21 +0545

    Donor class was added with its properties extending the base class along with search function for index page

--- User_management_system/donor.php
+++ User_management_system/donor.php
@@ -1,4 +1,6 @@
 <?php
+    require_once ('baseclass.php');?>
+<?php
     class Donor extends BaseClass
     {
         protected static $db_table = "donor";
@@ -8,8 +10,43 @@
         public $blood_group;
         public $contact_number;
         public $address;
+
+        public function get_address()
+        {
+            return $this->address;
+        }
+
+
+
+
+    public static function search_donor($by_blood, $by_address)
+    {
+        global $database;
+        $blood_group = $by_blood;
+        $address = $by_address;
+        $sql = "SELECT * FROM " . self::$db_table . " WHERE ";
+        $sql .= "blood_group = '{$blood_group}' ";
+        $sql .= "OR address = '{$address}'";
+        $result = $database->query($sql);
+        return $result;
+
+    }
+    public static function get_location()
+    {
+        global $database;
+        $sql = "SELECT DISTINCT address FROM " .self::$db_table;
+        $location = $database->query($sql);
+        return $location;
     }
 
+    public static function get_blood_group()
+    {
+        global $database;
+        $sql = "SELECT DISTINCT blood_group FROM " .self::$db_table;
+        $blood_group = $database->query($sql);
+        return $blood_group;
+    }
+}
 
 
 
