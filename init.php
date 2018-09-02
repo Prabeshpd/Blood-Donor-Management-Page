0f3a5b28f706228f4b8809ef5d5ed13c87c2d898
prabeshpd <prabesh1995nepalktm@gmail.com>
2018-09-02 10:59:34 +0545

    Function page was added

--- init.php
+++ init.php
@@ -5,4 +5,5 @@ require_once("./User_management_system/donor.php");
 require_once ("./User_management_system/adminuser.php");
 require_once ("./User_management_system/baseclass.php");
 require_once ("./session_manage/session.php");
+require_once ("./config/function.php");
 ?>
